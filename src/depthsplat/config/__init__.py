4f021bddd0241eeb17432397c21d38e401116333
David Pershouse <n11020211@qut.edu.au>
2025-01-12 16:10:48 +1000

    add splat head

--- src/depthsplat/config/__init__.py
+++ src/depthsplat/config/__init__.py
@@ -52,5 +52,37 @@ def init_pretrained_encoder_depth_only(size: Literal["small", "base", "large"]) 
 
     return encoder
 
+def init_pretrained_encoder(size: Literal["large", "base"]) -> EncoderDepthSplat:
+    
+    cfg = load_encoder_config(Path(__file__).parent / "encoder/depthsplat.yaml")
+
+    if size == "large":
+        path = "depthsplat-gs-large-re10k-256x256-288d9b26.pth"
+        cfg.num_scales=2 
+        cfg.upsample_factor=2 
+        cfg.lowest_feature_resolution=4 
+        cfg.monodepth_vit_type="vitl" 
+        cfg.gaussian_regressor_channels=64 
+        cfg.color_large_unet= True 
+        cfg.feature_upsampler_channels=128 
+    elif size == "base":
+        path = "depthsplat-gs-base-re10k-256x256-044fdb17.pth"
+        cfg.num_scales=2 
+        cfg.upsample_factor=2 
+        cfg.lowest_feature_resolution=4 
+        cfg.monodepth_vit_type="vitb" 
+        cfg.gaussian_regressor_channels=32 
+        cfg.color_large_unet= True 
+        cfg.feature_upsampler_channels=128 
+    
+    path = download_pretrained(path)
+    
+    encoder = EncoderDepthSplat(cfg)
+    state_dict = torch.load(path, weights_only=True)
+    # state keys are all prefixed with "encoder" so we need to remove that prefix
+    state_dict2 = {k.replace("encoder.", ""): v for k, v in state_dict['state_dict'].items()}
+    encoder.load_state_dict(state_dict2)
+    return encoder
+
 def download_pretrained(filename: str):
     return hf_hub_download(repo_id="haofeixu/depthsplat", filename=filename, local_dir="pretrained")
